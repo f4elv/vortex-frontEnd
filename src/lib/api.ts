d3e731c62fc7d862fbe6903bce3442bb750f29ce
Rafael <efael1986@gmail.com>
2025-12-04 17:39:45 -0300

    quase

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,15 +6,17 @@ export const api = axios.create({
 
 
 api.interceptors.request.use((config) => {
+  if (config.url?.includes("/admin/login")) {
+    return config;
+  }
 
   const token = typeof window !== "undefined" ? localStorage.getItem("access_token") : null;
 
-  // if (!token) {
-  //   window.location.href = "/admin/login";
-  //   return Promise.reject("Sem token");
-  // }
+  if (!token) {
+    window.location.href = "/admin/login";
+    return Promise.reject("Sem token");
+  }
 
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  config.headers.Authorization = `Bearer ${token}`;
   return config;
-
 });
