2cc57977adc571680d613484dc79c29f0b6ca2a6
Rafael Vasconcelos <efael1986@gmail.com>
2026-01-15 17:14:44 -0400

    remove api from site

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -5,77 +5,78 @@ import StepOne from "@/components/steps/stepOne";
 import StepTwo from "@/components/steps/stepTwo";
 import ConfirmationModal from "@/components/steps/confirmationModal";
 import Blur from "@/components/ui/blur";
-import { api } from "@/lib/api";
 import { formData } from "@/lib/utils";
+import { sub } from "motion/react-client";
 
 export default function MultiStepForm() {
-	const [step, setStep] = useState<1 | 2>(1);
-	const [data, setData] = useState<formData>({
-		name: "",
-		phone: "",
-		company: "",
-		projectDescription: "",
-		deadline: "",
-		budget: "",
-		projectType: "",
-		extraDetails: "",
-	});
-	const [showModal, setShowModal] = useState(false);
+  const [step, setStep] = useState<1 | 2>(1);
+  const [data, setData] = useState<formData>({
+    name: "",
+    phone: "",
+    company: "",
+    projectDescription: "",
+    deadline: "",
+    budget: "",
+    projectType: "",
+    extraDetails: "",
+  });
+  const [showModal, setShowModal] = useState(false);
 
-	function next() {
-		setStep(2);
-	}
+  function next() {
+    setStep(2);
+  }
 
-	function back() {
-		setStep(1);
-	}
+  function back() {
+    setStep(1);
+  }
 
-	function openModal() {
-		console.log("open modal");
-		setShowModal(true);
-	}
+  function openModal() {
+    console.log("open modal");
+    setShowModal(true);
+  }
 
-	function closeModal() {
-		setShowModal(false);
-	}
+  function closeModal() {
+    setShowModal(false);
+  }
 
-	function updateField(field: string, value: any) {
-		setData((prev) => ({
-			...prev,
-			[field]: value,
-		}));
-	}
+  function updateField(field: string, value: any) {
+    setData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  }
 
-	async function submit() {
-		try {
-			const response = await api.post("/user", data);
+  function submit() {
+    alert("Form submitted!");
+  }
 
-			console.log("Form enviado:", response.data);
-			alert("Formulário enviado com sucesso!");
-
-			setShowModal(false);
-		} catch (error) {
-			console.error(error);
-			alert("Erro ao enviar formulário");
-		}
-	}
-
-	return (
-		<section
-			id="Contact"
-			className="
+  return (
+    <section
+      id="Contact"
+      className="
         relative w-full min-h-screen
         flex items-center justify-center
         px-6 sm:px-12 md:px-20 lg:px-32
         py-10
       "
-		>
-			<Blur />
+    >
+      <Blur />
 
-			{step === 1 && <StepOne onContinue={next} updateField={updateField} data={data} />}
-			{step === 2 && <StepTwo onSubmit={openModal} onBack={back} updateField={updateField} data={data} />}
+      {step === 1 && (
+        <StepOne onContinue={next} updateField={updateField} data={data} />
+      )}
+      {step === 2 && (
+        <StepTwo
+          onSubmit={openModal}
+          onBack={back}
+          updateField={updateField}
+          data={data}
+        />
+      )}
 
-			{showModal && <ConfirmationModal onClose={closeModal} onSubmit={submit} />}
-		</section>
-	);
+      {showModal && (
+        <ConfirmationModal onClose={closeModal} onSubmit={submit} />
+      )}
+    </section>
+  );
 }
